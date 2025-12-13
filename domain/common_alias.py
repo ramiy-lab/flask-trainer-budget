3e124cf813129731fe104bf9a437707367c8234b
ramiy-star <heitianlu.01@outlook.jp>
2025-12-13 21:20:57 +0900

    add domain common type aliases

---
+++ domain/common_alias.py
@@ -0,0 +1,14 @@
+from typing import Typealias
+
+
+# --- 数値系 ---
+Gram: TypeAlias = float
+KCAL: TypeAlias = float
+Price: TypeAlias = int
+Ratio: TypeAlias = float
+
+
+# --- 識別子 ---
+FoodID: TypeAlias = str
+ProteinID: TypeAlias = str
+UserID: TypeAlias = str
